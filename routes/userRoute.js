3cf50f931c2c7af6ac96174034a8121af8153f68
Anand VM <anandnambissan2@gmail.com>
2022-05-29 21:02:29 +0530

    Video 31 compleeted

---
+++ routes/userRoute.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const User = require('../models/userModel');
+
+router.post("/register", (req, res) => {
+
+    User.find({email: req.body.email}, (err, docs) => {
+        if(docs.length > 0){
+            return res.status(400).json({message : 'Email ALready registerd'})
+        } else {
+                const newuser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password : req.body.password
+            })
+
+                newuser.save(err => {
+                if(!err) {
+                    res.send("User Registration Success")
+                } else {
+                    return res.status(400).json({message : 'Something went wrong'})
+                }
+            })
+        
+
+        }
+
+        if(err) {
+            return res.status(400).json({message : 'Something went wrong'})
+        }
+    })
+
+});
+
+
+router.post("/login", (req, res) => {
+
+    User.find({email: req.body.email, password: req.body.password}, (err, docs) => {
+        if(docs.length > 0)
+        {
+            res.send("Login Success")
+        } else {
+            return res.status(400).json({message : 'Invalid Credentials'})
+        }
+    });
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const bodyparser = require("body-parser");
 const app = express()
 var dbConnection = require('./db.js')
 var productsRoute = require('./routes/productRoute')
-
+var userRoute = require('./routes/userRoute');
 app.use(bodyparser.json());
+
 app.use('/api/products', productsRoute)
+app.use('/api/user', userRoute)
 
 app.get("/", (req,res) => {
     res.send("This is from back end")
